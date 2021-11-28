d5ef8da36f3c03390a3c01ef7a48fc523f09efab
Esteban Anfuso <esteban.anfuso@gmail.com>
2021-11-28 15:05:47 -0300

    primer ejercicio JA

---
+++ JA1/src/com/tvpublica/Caballo.java
@@ -0,0 +1,10 @@
+package com.tvpublica;
+
+public class Caballo extends Thread{
+	
+	@Override
+	public void run() {
+		System.out.println("Soy un caballo");
+	}
+
+}
---
+++ JA1/src/com/tvpublica/Prueba.java
@@ -0,0 +1,23 @@
+package com.tvpublica;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Holaaaa");
+		Caballo h = new Caballo();
+		
+		try {
+			h.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			e.getCause();
+		}
+		h.start();
+	
+		
+		
+	}
+
+}
